64896c85938880ca93749b094e38711d74364778
Alex - アレックス <klaidliadon@gmail.com>
2024-10-31 00:32:43 +0100

    handle slog.Attr correctly (#1)

--- logadapter_test.go
+++ logadapter_test.go
@@ -3,15 +3,19 @@ package logadapter
 import (
 	"errors"
 	"log/slog"
+	"os"
 	"testing"
 )
 
 func TestLogadapter(t *testing.T) {
-	log := LogAdapter(slog.Default())
+	log := LogAdapter(slog.New(slog.NewJSONHandler(os.Stderr, nil)))
 
 	log.Info("hi")
 	log.With("err", errors.New("uh oh"), "ps", "test", "num", 123).Warn("warnnn..")
 	log.Info("yes")
 
 	log.Warnf("failed due to %v", errors.New("uh oh"))
+	log.Info()
+	log.Info("done", "key", "value", "num", 123)
+	log.Info("done", slog.String("key", "value"), slog.Int("num", 123))
 }
